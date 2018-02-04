0a89c314dbef56b63d9fc2092b89585d0797087d
Kasper <kasper_karstensen@hotmail.com>
2018-02-04 13:05:46 +0100

    first commit

---
+++ SimpleDatabase/DBMS.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SimpleDatabase
+{
+    public class DBMS
+    {
+        private Dictionary<int, string> indices = new Dictionary<int, string>();
+
+        public DBMS()
+        {
+            StartUp();
+        }
+
+        public string ReadData(int key) 
+        {
+            string dataLocation;
+            indices.TryGetValue(key, out dataLocation);
+            char[] myData = "Index dosent exsist".ToCharArray();
+            if (dataLocation != null)
+            {
+                string[] data = dataLocation.Split(';');
+                int offset = int.Parse(data[0]);
+                int length = int.Parse(data[1]) - offset;
+                myData = new char[offset + length];
+
+                using (StreamReader sr = new StreamReader("Database.txt"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        sr.ReadBlock(myData, offset, length);
+                        /*   string line = sr.ReadLine(); //Brug readblock istedet
+                           myData = line.Substring(offset, length);*/
+                    }
+                }
+            }
+            return new string(myData).Replace(" ", String.Empty);
+        }
+
+        public void WriteData(int key, string value)
+        {
+            long startIndex = 0;
+            if (File.Exists("Database.txt"))
+            {
+                startIndex = new FileInfo("Database.txt").Length;
+            }
+            int endIndex = (int)startIndex + value.Length;
+            indices.Add(key, startIndex + ";" + endIndex);
+            File.AppendAllText("Database.txt",value);
+        }
+        
+        public void CloseDonw()
+        {
+            using (StreamWriter file = new StreamWriter("Indices.txt"))
+                foreach(var entry in indices)
+                {
+                    file.WriteLine("{0},{1}", entry.Key, entry.Value);
+                }
+        }
+
+        private void StartUp()
+        {
+            if (File.Exists("Indices.txt"))
+            {
+                string[] tempindices = File.ReadAllLines("Indices.txt");
+                foreach (string entry in tempindices)
+                {
+                    string[] values = entry.Split(',');
+                    indices.Add(int.Parse(values[0]), values[1]);
+                }
+            }
+
+        }
+    }
+}
---
+++ SimpleDatabase/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBMS dbms = new DBMS();
+            Console.WriteLine("Welcome to Simple DB");
+            bool running = true;
+            while (running)
+            {
+                string[] data = Console.ReadLine().Split(' ');
+                string dbCommand = data[0];
+                string value = "";
+                int key = 0;
+                if (dbCommand.Equals("db_write") || dbCommand.Equals("db_read"))
+                {
+                   bool passed = int.TryParse(data[1], out key);
+                    if (passed == false)
+                        dbCommand = "error";
+                }
+                if (dbCommand.Equals("db_write"))
+                    try
+                    {
+                        value = data[2];
+                    }catch(IndexOutOfRangeException ex)
+                    {
+                        dbCommand = "error";
+                    }
+                switch (dbCommand)
+                {
+                    case "db_write": dbms.WriteData(key, value);
+                        break;
+                    case "db_read": Console.WriteLine(dbms.ReadData(key));
+                        break;
+                    case "exit": running = false;
+                        break;
+                    default: Console.WriteLine("This is not a supported command");
+                        break;
+                }
+                dbms.CloseDonw();
+            }
+        }
+    }
+}
